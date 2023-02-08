ad704dd1c3adeb41e3b9849208be7e69b9f8ded5
eugin096@gmail.com <eugin.b@neurosoph.com>
2023-02-08 15:32:36 +0530

    Changed endpoint

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async(e) =>{
 
   //fetch data  from server
 
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://code-streak.onrender.com',{
     method:"POST",
     headers:{
       "content-Type":"application/json"
